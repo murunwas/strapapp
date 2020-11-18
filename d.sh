41c7fcf9e4055cb46ba21a5fdc7d990e3f3d191f
murunwas <cengyweb@gmail.com>
2020-11-18 14:25:21 +0200

    deploy commit

--- d.sh
+++ d.sh
@@ -1,6 +1,6 @@
 #git init
 git add .
-git commit -m "first commit"
+git commit -m "deploy commit"
 git branch -M main
-git remote add origin https://github.com/murunwas/strapi-demo.git
+#git remote add origin https://github.com/murunwas/strapi-demo.git
 git push -u origin main
